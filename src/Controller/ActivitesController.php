c9646099766e5570bceeb39eb744a66195d0da0d
RuddyV <107463504+RuddyV@users.noreply.github.com>
2022-08-30 15:32:27 +0200

    :zap: Affichage des questions et mise en place de la checkbox design

--- src/Controller/ActivitesController.php
+++ src/Controller/ActivitesController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Activites;
+use App\Form\ActivitesType;
+use App\Repository\ActivitesRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ActivitesController extends AbstractController
 {
-    #[Route('/activites', name: 'app_activites')]
-    public function index(): Response
+    #[Route('/activites', name: 'activites_index')]
+
+    public function index(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        ActivitesRepository $ActivitesRepository
+    ): Response
     {
-        return $this->render('activites/index.html.twig', [
-            'controller_name' => 'ActivitesController',
-        ]);
+        $formActivites = $this->createForm(ActivitesType::class);
+        $activites = $ActivitesRepository->findAll();
+        $formActivites->handleRequest($request);
+        return $this->renderForm('activites/index.html.twig', compact('formActivites','activites'));
     }
 }
