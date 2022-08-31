cc157fa417373bb41f299143fc8783f76bc54993
Sébastien Dijoux <sebastien.dijoux@outlook.fr>
2022-08-31 15:32:10 +0200

    envoie en base de données

--- src/Controller/ThemeAptitudesController.php
+++ src/Controller/ThemeAptitudesController.php
@@ -6,7 +6,9 @@ use App\Entity\Aptitudes;
 use App\Form\AptitudeType;
 
 
+use App\Form\PersonnaliteType;
 use App\Repository\AptitudesRepository;
+use App\Repository\RiasecRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,21 +17,53 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ThemeAptitudesController extends AbstractController
 {
-    #[Route('/aptitudes', name: 'aptitudes_index')]
+    #[Route('/aptitudes/{serie}', name: 'aptitudes_index')]
     public function index(
         Request $request,
         EntityManagerInterface $entityManager,
-        AptitudesRepository $aptitudeRepository
+        AptitudesRepository $aptitudeRepository,
+        RiasecRepository       $riasecRepository,
+        int                    $serie
     ): Response
     {
-        $aptitude = new Aptitudes();
-        $aptitudeForm = $this->createForm(AptitudeType::class, $aptitude);
-        $aptitude = $aptitudeRepository->findAll();
-        $aptitudeForm->handleRequest($request);
-        return $this->render('theme_aptitudes/index.html.twig', [
-            'controller_name' => 'ThemeAptitudesController',
-            'aptitudeForm' => $aptitudeForm->createView(),
-            'aptitude' => $aptitude
-        ]);
+
+        $formAptitudes = $this->createForm(AptitudeType::class);
+        $aptitudes = $aptitudeRepository->findBy(['serie' => $serie]);
+
+        $formAptitudes->handleRequest($request);
+        dump($_POST);
+        if ($_POST) {
+            dump(count($_POST));
+            $resultat = $riasecRepository->findOneBy(['id' => 1]);
+            switch ($serie) {
+                case 1 :
+                    $resultat->setR($resultat->getR() + count($_POST));
+                    break;
+                case 2 :
+                    $resultat->setI($resultat->getI() + count($_POST));
+                    break;
+                case 3 :
+                    $resultat->setA($resultat->getA() + count($_POST));
+                    break;
+                case 4 :
+                    $resultat->setS($resultat->getS() + count($_POST));
+                    break;
+                case 5 :
+                    $resultat->setE($resultat->getE() + count($_POST));
+                    break;
+                case 6 :
+                    $resultat->setC($resultat->getC() + count($_POST));
+                    break;
+            }
+            $entityManager->persist($resultat);
+            $entityManager->flush();
+            while ($serie < 6) {
+                return $this->redirectToRoute('aptitudes_index', ['serie' => $serie + 1]);
+            }
+            return $this->redirectToRoute('resultat_index');
+
+        }
+        return $this->renderForm('theme_aptitudes/index.html.twig', compact('aptitudes', 'serie'));
     }
+
 }
