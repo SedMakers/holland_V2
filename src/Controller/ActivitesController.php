b73e4555e0bbf79b2af1ebb91df85b1f98296cc7
RuddyV <107463504+RuddyV@users.noreply.github.com>
2022-08-31 15:55:40 +0200

    :pencil2: j'aime fonctionnalité

--- src/Controller/ActivitesController.php
+++ src/Controller/ActivitesController.php
@@ -29,36 +29,35 @@ class ActivitesController extends AbstractController
         $activites = $activiteRepository->findBy(['serie' => $serie]);
 
         $formActivites->handleRequest($request);
-        dump($_POST);
         if ($_POST) {
-            dump(count($_POST));
+
             $resultat = $riasecRepository->findOneBy(['id' => 1]);
             switch ($serie) {
                 case 1 :
-                    $resultat->setR($resultat->getR() + count($_POST));
+                    $resultat->setR($resultat->getR() + count($_POST) - 1);
                     break;
                 case 2 :
-                    $resultat->setI($resultat->getI() + count($_POST));
+                    $resultat->setI($resultat->getI() + count($_POST) - 1);
                     break;
                 case 3 :
-                    $resultat->setA($resultat->getA() + count($_POST));
+                    $resultat->setA($resultat->getA() + count($_POST) - 1);
                     break;
                 case 4 :
-                    $resultat->setS($resultat->getS() + count($_POST));
+                    $resultat->setS($resultat->getS() + count($_POST) - 1);
                     break;
                 case 5 :
-                    $resultat->setE($resultat->getE() + count($_POST));
+                    $resultat->setE($resultat->getE() + count($_POST) - 1);
                     break;
                 case 6 :
-                    $resultat->setC($resultat->getC() + count($_POST));
+                    $resultat->setC($resultat->getC() + count($_POST) - 1);
                     break;
             }
             $entityManager->persist($resultat);
             $entityManager->flush();
+
             if ($serie < 6) {
                 return $this->redirectToRoute('activites_index', ['serie' => $serie + 1]);
             }
-
             return $this->redirectToRoute('deux_index');
         }
         return $this->render('activites/index.html.twig', compact('activites', 'serie'));
