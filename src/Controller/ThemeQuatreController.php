666b70acc6ced1bbed6d66ced43e23618e4b319c
RuddyV <107463504+RuddyV@users.noreply.github.com>
2022-08-31 13:51:59 +0200

    :beers: mise en place Activite controller et redirection theme deux metier

--- src/Controller/ThemeQuatreController.php
+++ src/Controller/ThemeQuatreController.php
@@ -15,53 +15,53 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ThemeQuatreController extends AbstractController
 {
-    #[Route('/themequatre/{serie}', name: 'themeQuatre_index')]
-    public function index(
-        Request                $request,
-        EntityManagerInterface $entityManager,
-        PersonnaliteRepository $personnaliteRepository,
-        RiasecRepository       $riasecRepository,
-        int                    $serie
-    ): Response
-    {
+#[Route('/themequatre/{serie}', name: 'themeQuatre_index')]
+public function index(
+    Request                $request,
+    EntityManagerInterface $entityManager,
+    PersonnaliteRepository $personnaliteRepository,
+    RiasecRepository       $riasecRepository,
+    int                    $serie
+): Response
+{
 
 
-        $formPersonnalite = $this->createForm(PersonnaliteType::class);
-        $personnalite = $personnaliteRepository->findBy(['serie' => $serie]);
+    $formPersonnalite = $this->createForm(PersonnaliteType::class);
+    $personnalite = $personnaliteRepository->findBy(['serie' => $serie]);
 
-        $formPersonnalite->handleRequest($request);
-        dump($_POST);
-        if ($_POST) {
-            dump(count($_POST));
-            $resultat = $riasecRepository->findOneBy(['id' => 1]);
-            switch ($serie) {
-                case 1 :
-                    $resultat->setR($resultat->getR() + count($_POST));
-                    break;
-                case 2 :
-                    $resultat->setI($resultat->getI() + count($_POST));
-                    break;
-                case 3 :
-                    $resultat->setA($resultat->getA() + count($_POST));
-                    break;
-                case 4 :
-                    $resultat->setS($resultat->getS() + count($_POST));
-                    break;
-                case 5 :
-                    $resultat->setE($resultat->getE() + count($_POST));
-                    break;
-                case 6 :
-                    $resultat->setC($resultat->getC() + count($_POST));
-                    break;
-            }
-            $entityManager->persist($resultat);
-            $entityManager->flush();
-            while ($serie < 6) {
-                return $this->redirectToRoute('themeQuatre_index', ['serie' => $serie + 1]);
-            }
-            return $this->redirectToRoute('resultat_index');
-
+    $formPersonnalite->handleRequest($request);
+    dump($_POST);
+    if ($_POST) {
+        dump(count($_POST));
+        $resultat = $riasecRepository->findOneBy(['id' => 1]);
+        switch ($serie) {
+            case 1 :
+                $resultat->setR($resultat->getR() + count($_POST));
+                break;
+            case 2 :
+                $resultat->setI($resultat->getI() + count($_POST));
+                break;
+            case 3 :
+                $resultat->setA($resultat->getA() + count($_POST));
+                break;
+            case 4 :
+                $resultat->setS($resultat->getS() + count($_POST));
+                break;
+            case 5 :
+                $resultat->setE($resultat->getE() + count($_POST));
+                break;
+            case 6 :
+                $resultat->setC($resultat->getC() + count($_POST));
+                break;
         }
-        return $this->renderForm('theme_quatre/index.html.twig', compact('personnalite', 'serie'));
+        $entityManager->persist($resultat);
+        $entityManager->flush();
+        while ($serie < 6) {
+            return $this->redirectToRoute('themeQuatre_index', ['serie' => $serie + 1]);
+        }
+        return $this->redirectToRoute('resultat_index');
+
     }
+    return $this->renderForm('theme_quatre/index.html.twig', compact('personnalite', 'serie'));
+}
 }
