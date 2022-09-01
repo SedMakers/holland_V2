f95bbc3dfeb5607a1ce86087e9f0fb7007f8a8f5
RuddyV <107463504+RuddyV@users.noreply.github.com>
2022-09-01 14:54:33 +0200

    :construction: dashboard panel et fonction mailer en cours de creation

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -12,14 +12,17 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
-    public function configureFields(string $pageName): iterable
-    {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
-    }
-    */
+
+//    public function configureFields(string $pageName): iterable
+//    {
+//        return [
+//            IdField::new('id'),
+//            TextField::new('email'),
+//            TextEditorField::new('description'),
+//            ChoiceField::new('roles')->setChoices([
+//                'stagiaire' => 'ROLE_USER',
+//                'administrateur' => 'ROLE_ADMIN'
+//            ])->allowMultipleChoices()->renderExpanded()
+//        ];
+//    }
 }
